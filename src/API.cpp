8dd4a9897a4940b89a6cd1e8a3e66b7f7130a35f
Dennis Keck <pse_github@spam.denniskeck.de>
2014-02-13 10:38:14 +0100

    First version of register service

---
+++ src/API.cpp
@@ -0,0 +1,54 @@
+#include "ros/ros.h"
+1 #include <ros/console.h>
+#include "camera_application/Announce.h"
+
+#include <vector> 
+
+class API
+{
+    public:
+	API(int argc, char **argv):
+	{
+		ros::init(argc, argv, "api_server");
+		ros::NodeHandle n;
+		ros::ServiceServer service = n.advertiseService("announce", announce);
+		ROS_INFO("Ready to deliver IDs.");
+		ros::spin();
+		return 0;
+	}
+
+    private:
+	int idCounter = 0;
+	
+	//the ids of modules by category
+	vector<int[2]> cameras; //first value is the module id, second the camera id
+	vector<int> quadcopters;
+	vector<int> controllers;
+	vector<int> positions;
+	
+	bool announce(camera_application::Announce::Request  &req,
+		camera_application::Announce::Response &res)
+	{
+	    res.id = idCounter++;
+	    switch (req.type) {
+		case 0:
+		    cameras.push_back({res.id, req.camera_id});
+		    break;
+		case 1:
+		    quadcopters.push_back(res.id);
+		    break;
+		case 2:
+		    controllers.push_back(res.id);
+		    break;
+		case 3:
+		    positions.push_back(res.id);
+		    break;
+		default:
+		    ROS_ERROR("Malformed register attempt!");
+		    res.id = -1;
+		    return false;
+
+	    ROS_INFO("Registered new module with type %d and id %d", req.type, res.id);
+	    return true;
+	}
+}    
