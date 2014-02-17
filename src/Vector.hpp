28604b9290037aec1a33c00ec912040003cc0ab6
Luchs <Luchs@euirc.eu>
2014-02-17 11:07:27 +0100

    Add some documentation for kitrokopter::Vector

--- src/Vector.hpp
+++ src/Vector.hpp
@@ -10,10 +10,28 @@ class Vector {
 		const double x, y, z;
 
 	public:
+		/**
+		 * Constructs a vector with the given coordinates.
+		 *
+		 * @param x The first coordinate.
+		 * @param y The second coordinate.
+		 * @param z The third coordinate.
+		 */
 		Vector(double x, double y, double z) : x(x), y(y), z(z) {}
 
+		/**
+		 * @return The first coordinate.
+		 */
 		double getX() { return x; }
+
+		/**
+		 * @return The second coordinate.
+		 */
 		double getY() { return y; }
+
+		/**
+		 * @return The third coordinate.
+		 */
 		double getZ() { return z; }
 };
 
